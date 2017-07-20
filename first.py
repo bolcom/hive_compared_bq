26779871ea36dfa44ebbbdd9576208f5e131172b
Sourygna Luangsay <sluangsay@bol.com>
2017-07-20 10:25:59 +0200

    show graphical HTML results for GROUP BY diff
    Change-Id: Icdbfb5a9be7f473e913cc98c8e3957b6517c1aef

--- first.py
+++ first.py
@@ -1,14 +1,18 @@
 import pyhs2
 import logging
 import threading
+import difflib
+import webbrowser
 from collections import Counter
 from google.cloud import bigquery
 
-server = 'shd-hdp-a-master-003.bolcom.net'
-myDatabase = 'ldebruijn'
+server = 'shd-hdp-b-master-003.bolcom.net'
+#  myDatabase = 'ldebruijn'
+myDatabase = 'sluangsay'
 bq_database = 'bidwh2'
-#  myTable = 'ppp_retail_promotion_reference_group'
-myTable = 'PPP_retail_promotion'
+#  myTable = 'PPP_retail_promotion_reference_group'
+#  myTable = 'PPP_retail_promotion'
+myTable = 'hive_compared_bq_table2'
 
 # 10 000 rows should be good enough to use as a sample
 #  (Google uses some sample of 1000 or 3000 rows)
@@ -23,15 +27,33 @@ number_of_most_frequent_values_to_weight = 50
 number_of_group_by = 100000  # 7999 is the limit if you want to manually download the data from BQ. This limit does
 #  not apply in this script because we fetch the data with the Python API instead.
 
+logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s]\t[%(asctime)s]  (%(threadName)-10s) %(message)s',)
+
 fullHiveTable = myDatabase + "." + myTable
 fullBqTable = bq_database + "." + myTable
 conn = pyhs2.connect(host=server, port=10000, authMechanism="KERBEROS", database=myDatabase)
+bigquery_client = bigquery.Client()
+
+hash2_js_udf = '''create temp function hash2(v STRING)
+returns INT64
+LANGUAGE js AS """
+  var myHash = 0
+  for (let c of v){
+    myHash = myHash * 31 + c.charCodeAt(0)
+    if (myHash >= 4294967296){ // because in Hive hash() is computed on integers range
+      myHash = myHash % 4294967296
+    }
+  }
+  if (myHash >= 2147483648){
+    myHash = myHash - 4294967296
+  }
+  return myHash
+""";
+'''
 
 # TODO put this into a real object to have type check!?
 ddlColumns = []  # array instead of dictionary because we want to maintain the order of the columns
 ddlPartitions = []  # take care, those rows also appear in the columns array
-
-logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s]\t[%(asctime)s]  (%(threadName)-10s) %(message)s',)
 
 
 def get_table_ddl(table):
@@ -40,13 +62,11 @@ def get_table_ddl(table):
     cur = conn.cursor()
     cur.execute("describe " + table)
     while cur.hasMoreRows:
-        row = str(cur.fetchone())  # TODO check if we should not do fetchall instead, or other fetch batch
-        if row == 'None':
+        row = cur.fetchone()  # TODO check if we should not do fetchall instead, or other fetch batch
+        if row is None:
             continue
-        c_row = row.replace("[", "").replace("]", "").replace("'", "")
-        s_row = c_row.split(",")
-        col_name = s_row[0].strip()
-        col_type = s_row[1].strip()  # TODO can we use the fact that row is initially a list?
+        col_name = row[0]
+        col_type = row[1]
 
         if col_name == "" or col_name == "None":
             continue
@@ -86,13 +106,12 @@ def find_groupby_column(table):
     cur.execute(query)
     while cur.hasMoreRows:
         fetched = cur.fetchone()
-        if fetched is None:
-            continue
-        index = 0
-        for col in selected_columns:
-            value_column = fetched[index]
-            col["Counter"][value_column] += 1  # TODO what happens with NULL?
-            index += 1
+        if fetched is not None:
+            index = 0
+            for col in selected_columns:
+                value_column = fetched[index]
+                col["Counter"][value_column] += 1  # TODO what happens with NULL?
+                index += 1
     cur.close()
 
     #  Look at the statistics to estimate which column is the best to do a GROUP BY
@@ -124,25 +143,30 @@ def get_groupby_count_sql(hive_table, bq_table, column):
                  "FROM " + hive_table + " GROUP BY hash(" + column + ") % " + str(number_of_group_by)
     logging.debug("Hive query is: %s", hive_query)
 
-    bq_query = '''create temp function hash2(v STRING)
-returns INT64
-LANGUAGE js AS """
-  var myHash = 0
-  for (let c of v){
-    myHash = myHash * 31 + c.charCodeAt(0)
-    if (myHash >= 4294967296){ // because in Hive hash() is computed on integers range
-      myHash = myHash % 4294967296
-    }
-  }
-  if (myHash >= 2147483648){
-    myHash = myHash - 4294967296
-  }
-  return myHash
-""";
-    ''' + "SELECT MOD( hash2( " + column + "), " + str(number_of_group_by) + ") as gb, count(*) as count " \
-        "FROM " + bq_table + " GROUP BY gb ORDER BY gb"
+    bq_query = hash2_js_udf + "SELECT MOD( hash2(%s), %i) as gb, count(*) as count FROM %s GROUP BY gb ORDER BY gb" % \
+                              (column, number_of_group_by, bq_table)
     logging.debug("BigQuery query is: %s", bq_query)
     return hive_query, bq_query
+
+
+def query_bq(query):
+    """Execute the received query in BigQuery and return an iterate Result object"""
+    logging.debug("Launching BigQuery query")
+    q = bigquery_client.run_sync_query(query)
+    q.timeout_ms = 60000  # 1 minute to execute the query in BQ should be more than enough
+    q.use_legacy_sql = False  # TODO use maxResults https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query?
+    q.run()
+    logging.debug("Fetching BigQuery results")
+    return q.fetch_data()
+
+
+def query_hive(query):
+    """Execute the received query in Hive and return the cursor which is ready to be fetched and MUST be closed after"""
+    logging.debug("Launching Hive query")
+    cur = conn.cursor()
+    cur.execute(query)
+    logging.debug("Fetching Hive results")
+    return cur
 
 
 def compare_groupby_count(hive_table, bq_table, column):
@@ -158,21 +182,14 @@ def compare_groupby_count(hive_table, bq_table, column):
         logging.debug("Fetching Hive results")
         while cur.hasMoreRows:
             row = cur.fetchone()
-            if row is None:
-                continue
-            hive_count_dict[row[0]] = row[1]
+            if row is not None:
+                hive_count_dict[row[0]] = row[1]
         logging.debug("All %i Hive rows fetched", len(hive_count_dict.keys()))
         cur.close()
 
     def launch_bq():
         """Utility to Thread"""
-        logging.debug("Launching BigQuery query")
-        bigquery_client = bigquery.Client()
-        q = bigquery_client.run_sync_query(bq_query)
-        q.use_legacy_sql = False
-        q.run()
-        logging.debug("Fetching BigQuery results")
-        r = q.fetch_data()
+        r = query_bq(bq_query)
         for row in r:
             bq_count_dict[row[0]] = row[1]
         logging.debug("All %i BQ rows fetched", len(bq_count_dict.keys()))
@@ -204,8 +221,9 @@ def compare_groupby_count(hive_table, bq_table, column):
         elif v != small_dict[k]:
             differences[k] = -v - small_dict[k]
     summary_differences = [(k, -v, big_dict[k]) for (k, v) in differences.most_common()]
-    logging.info("We found at least %i differences in Group By count", len(summary_differences))
-    logging.debug("Differences in Group By count are: %s", summary_differences[:300])
+    if len(summary_differences) != 0:
+        logging.info("We found at least %i differences in Group By count", len(summary_differences))
+        logging.debug("Differences in Group By count are: %s", summary_differences[:300])
     return summary_differences, bq_biggest_distribution
 
 
@@ -213,7 +231,7 @@ def show_results_count(hive_table, bq_table, gb_column, differences, bq_biggest)
     """If any differences found in the Count Group By step, then show them to the user"""
     if len(differences) == 0:
         print "No differences where found when doing a Count on the tables %s and %s and grouping by on the column " \
-              "%s", hive_table, bq_table, gb_column
+              "%s" % (hive_table, bq_table, gb_column)
         return
     # We want to return at most 4 blocks of lines corresponding to different group by values. For the sake of brevity,
     # each block should not show more than 40 lines. Blocks that show rows only on BQ or only on Hive should be limited
@@ -257,9 +275,56 @@ def show_results_count(hive_table, bq_table, gb_column, differences, bq_biggest)
     hive_query = "SELECT hash(%s) %% %i as bucket, %s, %s FROM %s WHERE hash(%s) %% %i IN (%s)" \
                  % (gb_column, number_of_group_by, gb_column, extra_columns_str, hive_table, gb_column,
                     number_of_group_by, str(buckets_hive)[1:-1])
-
     logging.debug("Hive query to show Group By Count differences is: %s", hive_query)
+    bq_query = hash2_js_udf + "SELECT MOD( hash2(%s), %i) as bucket, %s, %s FROM %s WHERE MOD( hash2(%s), %i) IN (%s)" \
+                              % (gb_column, number_of_group_by, gb_column, extra_columns_str, bq_table,
+                                 gb_column, number_of_group_by, str(buckets_bq)[1:-1])
+    logging.debug("BQ query to show Group By Count differences is: %s", bq_query)
 
+    def launch_hive():
+        """Utility to Thread"""
+        cur = query_hive(hive_query)
+        while cur.hasMoreRows:
+            row = cur.fetchone()
+            if row is not None:
+                line = "^ " + " | ".join([str(x) for x in row]) + " $"
+                hive_lines.append(line)
+        logging.debug("All %i Hive rows fetched", len(hive_lines))
+        cur.close()
+
+    def launch_bq():
+        """Utility to Thread"""
+        r = query_bq(bq_query)
+        for row in r:
+            line = "^ " + " | ".join([str(x) for x in row]) + " $"
+            bq_lines.append(line)
+        logging.debug("All %i BQ rows fetched", len(bq_lines))
+
+    bq_lines = []
+    hive_lines = []
+    t_bq = threading.Thread(name='bqShowCountDifferences', target=launch_bq)
+    t_hive = threading.Thread(name='hiveShowCountDifferences', target=launch_hive)
+    t_bq.start()
+    t_hive.start()
+    t_bq.join()
+    t_hive.join()
+
+    bq_lines.sort()
+    bq_text = "\n".join(bq_lines)
+    bq_file = "/tmp/count_diff_bq"
+    with open(bq_file, "w") as f:
+        f.write(bq_text)
+    hive_lines.sort()
+    hive_text = "\n".join(hive_lines)
+    hive_file = "/tmp/count_diff_hive"
+    with open(hive_file, "w") as f:
+        f.write(hive_text)
+    logging.debug("Sorted results of the queries are in the files %s and %s", hive_file, bq_file)
+    diff = difflib.HtmlDiff().make_file(hive_lines, bq_lines, "hive", "BigQuery", context=False, numlines=30)
+    html_file = "/tmp/count_diff.html"
+    with open(html_file, "w") as f:
+        f.write(diff)
+    webbrowser.open("file://" + html_file, new=2)
 
 get_table_ddl(fullHiveTable)
 group_by_column = find_groupby_column(fullHiveTable)
